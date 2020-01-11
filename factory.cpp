17b3703963daf4a129e7ebef1dc67b867b71993c
THEproVANO <47854658+THEproVANO@users.noreply.github.com>
2020-01-11 20:47:38 +0300

    Add files via upload

--- factory.cpp
+++ factory.cpp
@@ -2,38 +2,24 @@
 
 #include "factory.h"
 
-void factory::construct(std::unique_ptr<document> &doc1)
+void factory::construct(std::unique_ptr<document> &doc1, std::string figures)
 {
-    std::string figures;
-    std::cout << "Input the number of figure for adding: \n";
-    std::cout << "Pentagon: 5\n";
-    std::cout << "Hexagon: 6\n";
-    std::cout << "Octagon: 8\n";
-    std::cin >> figures;
-    if (figures != "5" && figures != "6" && figures != "8")
-    {
-        std::cout << "Wrong input!\n";
-        return;
-    }
     size_t id;
     std::cout << "Input the index for input: ";
     std::cin >> id;
     if(figures == "5")
     {
-        std::unique_ptr<figure> new_figure;
-        new_figure=std::make_unique<five_angles>(five_angles(std::cin));
+        std::unique_ptr<figure> new_figure = std::make_unique<five_angles>(five_angles(std::cin));
         doc1->add_figure(std::move(new_figure),id);
     }
     else if(figures == "6")
     {
-        std::unique_ptr<figure> new_figure;
-        new_figure=std::make_unique<six_angles>( six_angles(std::cin));
+        std::unique_ptr<figure> new_figure = std::make_unique<six_angles>( six_angles(std::cin));
         doc1->add_figure(std::move(new_figure),id);
     }
     else if(figures == "8")
     {
-        std::unique_ptr<figure> new_figure;
-        new_figure = std::make_unique<eight_angles>( eight_angles(std::cin));
+        std::unique_ptr<figure> new_figure = std::make_unique<eight_angles>( eight_angles(std::cin));
         doc1->add_figure(std::move(new_figure),id);
     }
 }
---
+++ factory.h
@@ -0,0 +1,18 @@
+#ifndef D_FACTORY_H_
+#define D_FACTORY_H_
+
+#include "document.h"
+#include "figure.h"
+#include "five_angles.h"
+#include "six_angles.h"
+#include "eight_angles.h"
+#include <vector>
+#include <memory>
+#include <iostream>
+
+struct factory
+{
+    void construct(std::unique_ptr<document>& vec,std::string);
+};
+
+#endif
